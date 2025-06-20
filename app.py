500ab89a28e28e1338b3e0fb4701900bc24f4d80
Paulo Sérgio Ribeiro Júnior <psrjunior23@gmail.com>
2025-06-19 22:23:32 -0400

    Primeiro commit - Estrutura do projeto Vision API

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from google.cloud import vision
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'iautismo-service-account.json'
+
+app = Flask(__name__)
+
+@app.route('/analyze-image', methods=['POST'])
+def analyze_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded'}), 400
+
+    image_file = request.files['image']
+    image_content = image_file.read()
+    
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image(content=image_content)
+    response = client.label_detection(image=image)
+    labels = [label.description for label in response.label_annotations]
+    return jsonify({'labels': labels})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
